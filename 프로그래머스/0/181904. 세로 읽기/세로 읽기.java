60cd2bc8ad43f64d95e04072bd38d7bd20865bf4
Yewon Cho <qjqmti0113@gmail.com>
2024-06-13 23:40:52 +0900

    [level 0] Title: 세로 읽기, Time: 8.22 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,9 +1,10 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 class Solution {
     public String solution(String my_string, int m, int c) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = c-1; i < my_string.length(); i+=m) {
-            sb.append(my_string.charAt(i));
-        }
-        return sb.toString();
+        return IntStream.range(0, my_string.length())
+                .filter(i -> i % m == c - 1)
+                .mapToObj(i -> my_string.charAt(i)+"")
+                .collect(Collectors.joining());
     }
 }
