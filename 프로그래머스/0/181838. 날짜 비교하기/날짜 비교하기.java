b3af09b07e256841d3afb58b8280f3a151bbc9ca
Yewon Cho <qjqmti0113@gmail.com>
2024-06-15 00:16:12 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.86 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,11 +1,8 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Arrays;
 class Solution {
-    public int solution(int[] date1, int[] date2) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateA = sdf.parse(""+date1[0]+"-"+date1[1]+"-"+date1[2]);
-        Date dateB = sdf.parse(""+date2[0]+"-"+date2[1]+"-"+date2[2]);
-        return dateA.before(dateB) ? 1 : 0;
+    public int solution(int[] date1, int[] date2) {
+        String dateA = String.format("%04d%02d%02d", date1[0], date1[1], date1[2]);
+        String dateB = String.format("%04d%02d%02d", date2[0], date2[1], date2[2]);
+        return dateA.compareTo(dateB) < 0 ? 1 : 0;
     }
 }
