5342562451b254112baedc7bbde8b91fd0eaac29
Yewon Cho <qjqmti0113@gmail.com>
2024-06-16 01:01:15 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,11 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int[][] arr = new int[num_list.length/n][n];
+        int[][] arr = new int[num_list.length / n][n];
         int idx = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = num_list[idx++];
-            }
+        for (int i = 0; i <= num_list.length - 1; i += n) {
+            arr[idx++] = Arrays.copyOfRange(num_list, i, i + n);
         }
         return arr;
     }
