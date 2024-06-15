9d032d66720f28ff3bc4ea7888db524930849d70
Yewon Cho <qjqmti0113@gmail.com>
2024-06-15 20:19:45 +0900

    [level 0] Title: 글자 지우기, Time: 0.40 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,14 +1,9 @@
-import java.util.Arrays;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
-        Arrays.sort(indices);
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        for (int i = 0; i < my_string.length(); i++) {
-            if(i==indices[idx]) idx += idx<indices.length-1 ? 1 : 0;
-            else sb.append(my_string.charAt(i));
+        String[] strArr = my_string.split("");
+        for (int i = 0; i < indices.length; i++) {
+            strArr[indices[i]] = "";
         }
-        return sb.toString();
+        return String.join("", strArr);
     }
 }
