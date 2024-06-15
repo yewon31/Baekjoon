7b42e088fe8c277578a2ad390e26ccdfa893ce4e
Yewon Cho <qjqmti0113@gmail.com>
2024-06-15 21:05:31 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,13 +1,9 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String[] strArr = my_string.split("");
-        int cnt = (e-s+1)/2;
-        String tmp = "";
-        while(cnt-- > 0) {
-            tmp = strArr[s];
-            strArr[s++] = strArr[e];
-            strArr[e--] = tmp;
-        }
-        return String.join("", strArr);
+        StringBuilder str = new StringBuilder(my_string);
+        StringBuilder word = new StringBuilder(my_string.substring(s, e+1));
+        str.delete(s, e+1);
+        str.insert(s, word.reverse());
+        return str.toString();
     }
 }
