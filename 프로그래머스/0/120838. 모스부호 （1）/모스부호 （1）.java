df0063e7f3a015e92c2928f5efadba5e88376b22
Yewon Cho <qjqmti0113@gmail.com>
2024-06-16 01:22:56 +0900

    [level 0] Title: 모스부호 (1), Time: 2.12 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String letter) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put(".-", "a");     map.put("-...", "b");   map.put("-.-.", "c");   map.put("-..", "d");
+        map.put(".", "e");      map.put("..-.", "f");   map.put("--.", "g");    map.put("....", "h");
+        map.put("..", "i");     map.put(".---", "j");   map.put("-.-", "k");    map.put(".-..", "l");
+        map.put("--", "m");     map.put("-.", "n");     map.put("---", "o");    map.put(".--.", "p");
+        map.put("--.-", "q");   map.put(".-.", "r");    map.put("...", "s");    map.put("-", "t");
+        map.put("..-", "u");    map.put("...-", "v");   map.put(".--", "w");    map.put("-..-", "x");
+        map.put("-.--", "y");   map.put("--..", "z");
+        return Arrays.stream(letter.split(" "))
+                .map(key -> map.get(key))
+                .collect(Collectors.joining());
+    }
+}
