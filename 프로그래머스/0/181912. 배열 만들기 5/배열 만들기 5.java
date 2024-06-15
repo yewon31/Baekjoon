3b1cb134132e0eff8efbfc8050239002863a49a5
Yewon Cho <qjqmti0113@gmail.com>
2024-06-15 21:37:23 +0900

    [level 0] Title: 배열 만들기 5, Time: 7.77 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,14 +1,10 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        List<Integer> list = new ArrayList<>();
-        for (String intStr : intStrs) {
-            int num = Integer.parseInt(intStr.substring(s, s+l));
-            if(num > k) list.add(num);
-        }
-        return list.stream().mapToInt(Integer::valueOf).toArray();
+        return Arrays.stream(intStrs)
+                .mapToInt(value -> Integer.parseInt(value.substring(s, s + l)))
+                .filter(value -> value > k)
+                .toArray();
     }
 }
