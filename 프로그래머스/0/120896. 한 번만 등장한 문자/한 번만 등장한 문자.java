0661de9691f1928181b08ea1ec6ed606349ae727
Yewon Cho <qjqmti0113@gmail.com>
2024-06-17 12:05:55 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 6.79 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String s) {
-        List<String> list = new ArrayList<>();
-        for (char ch : s.toCharArray()) {
-            if (s.indexOf(ch) == s.lastIndexOf(ch)) list.add(ch + "");
-        }
-        String[] arr = list.toArray(new String[0]);
-        Arrays.sort(arr);
-        return String.join("", arr);
+        return Arrays.stream(s.split(""))                   //1.stream으로 변환
+                .collect(Collectors.groupingBy(s1 -> s1))   //2.map 구조로 grouping(문자별로 그룹화, "a"->"a,a")
+                .entrySet().stream()                        //3.map을 entrySet stream으로 변환
+                .filter(entry -> entry.getValue().size() == 1) //4.getValue().size() 이용, 한번만 등장한 문자그룹 필터링
+                .map(Map.Entry::getKey)                     //5.해당 문자만 추출
+                .sorted().collect(Collectors.joining());    //6.정렬 후 문자열로 변환
     }
 }
