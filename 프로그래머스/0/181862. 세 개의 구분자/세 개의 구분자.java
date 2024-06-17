39e6350cb847f320e90584c38d8e35dc0b285324
Yewon Cho <qjqmti0113@gmail.com>
2024-06-17 22:09:20 +0900

    [level 0] Title: 세 개의 구분자, Time: 35.61 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String myStr) {
-        String[] arr = myStr.replaceAll("[abc]", " ").replaceAll("[ ]+", " ").trim().split(" ");
-        String[] emptyArr = {"EMPTY"};
-        return arr[0].equals("") ? emptyArr : arr;
+        String[] arr = myStr.replaceAll("[abc]+", " ").trim().split(" ");
+        return arr[0].equals("") ? new String[]{"EMPTY"} : arr;
     }
 }
