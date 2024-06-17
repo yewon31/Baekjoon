6bd3eb9fb529f8490bf6fdfc7ee31dd8e0833430
Yewon Cho <qjqmti0113@gmail.com>
2024-06-17 22:50:48 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.41 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] sortedArr = Arrays.copyOf(emergency, emergency.length);
+        Arrays.sort(sortedArr);
+        for (int i = 0; i < emergency.length; i++) {
+            int order = 0;
+            for (int j = 0; j < sortedArr.length; j++) {
+                if (emergency[i] == sortedArr[j]) {
+                    order = j;
+                    break;
+                }
+            }
+            emergency[i] = emergency.length - order;
+        }
+        return emergency;
+    }
+}
