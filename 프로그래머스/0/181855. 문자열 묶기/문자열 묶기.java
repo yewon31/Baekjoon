7b502e99c9cc64488f0f7edc4f99245ffd73394d
Yewon Cho <qjqmti0113@gmail.com>
2024-06-19 00:20:19 +0900

    [level 0] Title: 문자열 묶기, Time: 6.03 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,15 +1,10 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public int solution(String[] strArr) {
-        /****** 길이순 정렬 ******/
-        String[] sortedArr = Arrays.stream(strArr)
-                .sorted(Comparator.comparingInt(n -> n.length())) //사용자 정의 비교 기준 제공
-                .toArray(String[]::new);
         /****** 개수 저장 배열 생성 ******/
-        int maxLength = sortedArr[sortedArr.length - 1].length(); //정렬된 배열의 마지막 값의 길이
-        int[] cntArr = new int[maxLength+1]; //1번 index에 길이 1 개수 저장
+        int maxLength = 30; //strArr의 원소의 길이 최댓값
+        int[] cntArr = new int[maxLength + 1]; //1번 index에 길이 1 개수 저장
         /****** 개수 카운트 ******/
         for (String str : strArr) {
             cntArr[str.length()]++;
