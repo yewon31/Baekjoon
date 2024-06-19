23e751d07c6fecc77553b738454a8b19355a0697
Yewon Cho <qjqmti0113@gmail.com>
2024-06-19 21:20:51 +0900

    [level 0] Title: 리스트 자르기, Time: 3.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
@@ -13,16 +13,9 @@ class Solution {
             case 1 : return Arrays.copyOf(num_list, b+1);
             case 2 : return Arrays.copyOfRange(num_list, a, num_list.length);
             case 3 : return Arrays.copyOfRange(num_list, a, b+1);
-            case 4 :
-                List<Integer> list = new ArrayList<>();
-                for (int i = a; i <= b; i += c) {
-                    list.add(num_list[i]);
-                }
-                int[] result = new int[list.size()];
-                for (int i = 0; i < list.size(); i++) {
-                    result[i] = list.get(i);
-                }
-                return result;
+            case 4 : return IntStream.iterate(a, i -> i <= b, i -> i + c)
+                            .map(i -> num_list[i])
+                            .toArray();
         }
         return new int[0];
     }
