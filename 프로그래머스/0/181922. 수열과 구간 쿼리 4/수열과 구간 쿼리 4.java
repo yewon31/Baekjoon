6482b0b93682e0d28d21b8095477f97ae62a2bac
Yewon Cho <qjqmti0113@gmail.com>
2024-06-19 22:15:39 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 7.36 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -6,8 +6,13 @@ class Solution {
             int s = query[0];
             int e = query[1];
             int k = query[2];
-            for (int i = s; i <= e; i++) {
-                if (i % k == 0) arr[i]++;
+            if (k == 0) continue; //0의 배수
+            if (s % k != 0) { //s가 k의 배수가 아닐 경우
+                s = (s + k) - (s % k); //s보다 큰 k의 배수
+                System.out.println(s);
+            }
+            for (int i = s; i <= e; i += k) { //k의 배수 인덱스
+                arr[i]++;
             }
         }
         return arr;
