fb074fa9af0829db253b291a4e8c8e8ea37095c4
Yewon Cho <qjqmti0113@gmail.com>
2024-06-20 10:07:26 +0900

    [level 0] Title: 소인수분해, Time: 2.32 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        Set<Integer> set = new LinkedHashSet<>(); //중복x 순서o
-        
-        int i = 2;
-        while (n != 0 && i <= Math.sqrt(n)) {
-            if (n % i == 0) { // i는 약수
-                set.add(i);
-                n /= i;
-            } else i++;
+        // 소인수 - 약수 중에서 소수만 구하기
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i <= n; i++) { // 2 ~ n
+            if (n % i == 0 && isPrime(i)) { // i는 약수이자 소수
+                list.add(i);
+            }
         }
-        if (n > 1) set.add(n);
+        if (list.isEmpty()) list.add(n); // 약수가 없다면 n의 소인수는 n뿐
+        return list.stream().mapToInt(Integer::intValue).toArray(); // 배열로 변환해서 반환
+    }
 
-        return set.stream().mapToInt(Integer::intValue).toArray();
+    public boolean isPrime(int n) {
+        for (int j = 2; j <= Math.sqrt(n); j++) {
+            if (n % j == 0) return false; // 약수가 있다면 소수x
+        }
+        return true;
     }
 }
