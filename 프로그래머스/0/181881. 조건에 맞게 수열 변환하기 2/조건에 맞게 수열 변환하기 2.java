9bcbd876909781b6dec2a3310eb5d0e2c0c723d2
Yewon Cho <qjqmti0113@gmail.com>
2024-06-20 22:55:42 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int solution(int[] arr) {
-        int cnt = 0; // 연산 횟수
-        while (true) {
-            boolean flag = false; // 연산 여부
-            /****** 배열 순회하면서 연산 ******/
-            for (int i = 0; i < arr.length; i++) {
-                int num = arr[i];
-                if ((num >= 50) && (num % 2 == 0)) {
-                    flag = true;
-                    arr[i] = num / 2;
-                } else if ((num < 50) && (num % 2 == 1)) {
-                    flag = true;
-                    arr[i] = num * 2 + 1;
-                }
+public int solution(int[] arr) {
+    int cnt = 0; // 연산 횟수
+    while (true) {
+        boolean flag = false; // 연산 여부
+        /****** 배열 순회하면서 연산 ******/
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            if ((num >= 50) && (num % 2 == 0)) {
+                flag = true;
+                arr[i] = num / 2;
+            } else if ((num < 50) && (num % 2 == 1)) {
+                flag = true;
+                arr[i] = num * 2 + 1;
             }
-            /****** 연산을 안했다면 탈출 ******/
-            if (!flag) break;
-            cnt++;
         }
-        return cnt;
+        /****** 연산을 안했다면 탈출 ******/
+        if (!flag) break;
+        cnt++;
     }
+    return cnt; //최종 연산 횟수 반환
+}
 }
