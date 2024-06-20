6cddeb39fa0283704158e958bd4c971686afb7fa
Yewon Cho <qjqmti0113@gmail.com>
2024-06-20 22:30:40 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,8 +2,7 @@ class Solution {
     public int solution(String[] order) {
         int price = 0;
         for (String menu : order) {
-            if (menu.contains("cafelatte")) price += 5000; //카페라떼
-            else price += 4500; //아메리카노 or 아무거나
+            price += menu.contains("latte") ? 5000 : 4500;
         }
         return price;
     }
