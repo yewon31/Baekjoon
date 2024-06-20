851c339395f23c40b5a2d037e2acd8ddd94168e6
Yewon Cho <qjqmti0113@gmail.com>
2024-06-20 23:39:34 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
 class Solution {
     public int solution(String s) {
-        int sum = Arrays.stream(s.split("[Z ]+")).mapToInt(Integer::parseInt).sum(); //모든 숫자 합계
         String[] arr = s.split(" ");
-        for (int i = arr.length - 1; i > 0; i--) {
-            if (arr[i].equals("Z")) sum -= Integer.parseInt(arr[--i]);
+        int sum = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].equals("Z")) i--; //건너뛰기
+            else sum += Integer.parseInt(arr[i]);
         }
         return sum;
     }
