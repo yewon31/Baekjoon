9961cdc9142d688a38b6440f1a0ce49161bbc67d
Yewon Cho <qjqmti0113@gmail.com>
2024-06-21 23:14:27 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,24 +2,13 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        List<String> list = new ArrayList<>();
         for (int i = 0; i < str_list.length; i++) {
-            if (str_list[i].equals("l")) {
-                // if (str_list[0].equals("l")) break;
-                if (i == 0) break; // 첫 등장하는 l의 인덱스가 첫번째 인덱스라면
-                for (int j = 0; j < i; j++) {
-                    list.add(str_list[j]);
-                }
-                break;
-            } else if (str_list[i].equals("r")) {
-                // if (str_list[str_list.length - 1].equals("r")) break;
-                if (i == str_list.length - 1) break; // 첫 등장하는 r의 인덱스가 마지막 인덱스라면
-                for (int j = i + 1; j < str_list.length; j++) {
-                    list.add(str_list[j]);
-                }
-                break;
+            if ("l".equals(str_list[i])) {
+                return Arrays.copyOf(str_list, i);
+            } else if ("r".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        return list.stream().toArray(String[]::new);
+        return new String[0];
     }
 }
