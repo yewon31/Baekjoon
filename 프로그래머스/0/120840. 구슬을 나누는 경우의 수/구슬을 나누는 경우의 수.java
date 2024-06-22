7594e0d3a75e02aa3a928b6d9f7d279016e6be51
Yewon Cho <qjqmti0113@gmail.com>
2024-06-23 02:21:06 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int balls, int share) {
-        if (balls == share || share == 0) return 1;
-        return solution((balls - 1), (share - 1)) + solution(balls - 1, share);
+        int[][] DP = new int[balls + 1][share + 1];
+        for (int i = 0; i <= balls; i++) {
+            for (int j = 0; j <= Math.min(i, share); j++) {
+                if (j == 0 || j == i) DP[i][j] = 1;
+                else DP[i][j] = DP[i - 1][j] + DP[i - 1][j - 1];
+            }
+        }
+        return DP[balls][share];
     }
 }
