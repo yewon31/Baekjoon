8f14e1228aecf379102311060df5cd5751ae3bfe
Yewon Cho <qjqmti0113@gmail.com>
2024-06-22 22:13:55 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,12 +1,8 @@
 class Solution {
     // 문자열에 따라 두 수의 크기 비교
     public int solution(String ineq, String eq, int n, int m) {
-        if (ineq.equals(">")) {
-            if (eq.equals("=")) return n >= m ? 1 : 0;
-            else return n > m ? 1 : 0;
-        } else { // <
-            if (eq.equals("=")) return n <= m ? 1 : 0;
-            else return n < m ? 1 : 0;
-        }
+        boolean B1 = ">".equals(ineq) ? n > m : n < m; // 대소 비교
+        boolean B2 = "=".equals(eq) ? n == m : false; // 동등 비교
+        return B1 || B2 ? 1 : 0;
     }
 }
