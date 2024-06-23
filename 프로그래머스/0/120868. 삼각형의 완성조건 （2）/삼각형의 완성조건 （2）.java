bf1a371910abce00345a942d3c0c78471ff05dee
Yewon Cho <qjqmti0113@gmail.com>
2024-06-23 23:04:52 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,23 +1,21 @@
 class Solution {
-    
-    // 삼각형의 두 변의 길이가 주어질 때, 나머지 한 변이 될 수 있는 정수의 개수
-    public int solution(int[] sides) { // 1 ≤ sides의 원소 ≤ 1,000
 
-        // 변수 세팅
+    // 삼각형의 두 변의 길이가 주어질 때, 나머지 한 변이 될 수 있는 정수의 개수
+    public int solution(int[] sides) {
+
+        /****** 변수 세팅 ******/
         int x = Math.max(sides[0], sides[1]); // 두 변의 길이 중 큰 값
         int y = Math.min(sides[0], sides[1]); // 두 변의 길이 중 작은 값
-        int z; // 나머지 한 변의 길이
-        int cnt = 0;
 
-        // x가 가장 길 경우
-        z = x;
-        while (z <= x && x < y + z--) cnt++;
+        /****** 나머지 한 변 길이의 최솟값과 최댓값 ******/
+        int min = x - y + 1;
+        int max = x + y - 1;
 
-        // 나머지 한 변(z)이 가장 긴 변인 경우
-        z = x + 1;
-        while (z > x && z++ < x + y) cnt++;
+        // 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작다.
+        // (x가 가장 크다면) x < y + z // z > x - y // z의 최솟값 x - y + 1
+        // (z가 가장 크다면) z < x + y // z < x + y // z의 최댓값은 x + y - 1
 
-        // 반환
-        return cnt;
+        /****** 개수 반환 ******/
+        return max - min + 1;
     }
 }
