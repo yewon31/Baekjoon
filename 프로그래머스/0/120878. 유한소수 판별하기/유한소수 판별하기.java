9a8e52645d7c15408088191ce28e05d2cc93ae01
Yewon Cho <qjqmti0113@gmail.com>
2024-06-26 22:16:45 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    // a/b가 유한소수라면 1, 무한소수라면 2 반환
+    public int solution(int a, int b) {
+        /****** a/b 를 기약분수로 나타내기 위해 최대공약수로 나눠주기 ******/
+        b /= gcd(a, b);
+        /****** 분모를 2와 5로 나누어주기 ******/
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+        /****** 분모의 소인수가 2와 5만 존재한다면 유한소수 ******/
+        return b == 1 ? 1 : 2;
+    }
+    public int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
