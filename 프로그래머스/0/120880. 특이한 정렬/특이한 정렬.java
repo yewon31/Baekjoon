3020adb77463768ab88a13e06e4eb37e8f972c62
Yewon Cho <qjqmti0113@gmail.com>
2024-06-27 00:41:36 +0900

    [level 0] Title: 특이한 정렬, Time: 4.38 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,19 +1,12 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    // numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열 반환
     public int[] solution(int[] numlist, int n) {
-        int[] arr = new int[numlist.length];
-        for (int i : numlist) {
-            int rank = 0;
-            int ii = Math.abs(n - i);
-            for (int j : numlist) {
-                int jj = Math.abs(n - j);
-                if (ii > jj) rank++;
-                else if (ii == jj && j > i) rank++;
-            }
-            arr[rank] = i;
-        }
-        return arr;
+        return Arrays.stream(numlist)
+                .boxed() // Comparator 인터페이스를 사용하기 위해 IntStream을 Stream<Integer>로 변환
+                .sorted(Comparator.reverseOrder())                      // 역순 정렬
+                .sorted(Comparator.comparing(num -> Math.abs(num - n))) // n과의 차이를 기준으로 정렬
+                .mapToInt(i -> i)   // int[]로 변환하기 위해 IntStream으로 우선 변환
+                .toArray();         // int[]로 변환
     }
 }
