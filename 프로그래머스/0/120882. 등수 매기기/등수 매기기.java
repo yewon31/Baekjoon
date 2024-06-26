8ea4fe2cdf323e5a8c89393be52b9ae2fcf75f5b
Yewon Cho <qjqmti0113@gmail.com>
2024-06-26 14:18:00 +0900

    [level 0] Title: 등수 매기기, Time: 5.24 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -7,11 +7,11 @@ class Solution {
     // score - 학생 별 점수 2개
     // 점수 평균 기준으로 매긴 등수를 담은 배열 반환
     public int[] solution(int[][] score) {
-        // 평균만 담은 list 생성
-        List<Double> list = Arrays.stream(score).map(student -> (double)(student[0] + student[1]) / 2).collect(Collectors.toList());
-        // 정렬한 list 생성
-        List<Double> listSorted = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        // 점수 합계만 담은 list 생성
+        List<Integer> avglist = Arrays.stream(score).map(student -> student[0] + student[1]).collect(Collectors.toList());
+        // 역순 정렬한 list 생성
+        List<Integer> avglistSorted = avglist.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         // 등수 배열 반환
-        return list.stream().mapToInt(i -> listSorted.indexOf(i) + 1).toArray();
+        return avglist.stream().mapToInt(i -> avglistSorted.indexOf(i) + 1).toArray();
     }
 }
