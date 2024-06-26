a82302183244d13a7f89b35550b833b25b73f614
Yewon Cho <qjqmti0113@gmail.com>
2024-06-26 21:24:03 +0900

    [level 0] Title: 저주의 숫자 3, Time: 7.39 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,17 +1,13 @@
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 class Solution {
     // 정수 n을 3x 마을에서 사용하는 숫자로 바꿔서 반환(3과 3의 배수는 사용x)
-public int solution(int n) {
-    int num = 1;
-    int cnt = 1;
-    while (cnt < n) {
-        cnt++;
-        num++;
-        while (num % 3 == 0 || (num + "").indexOf("3") > -1) { // 3의 배수이거나 3이 들어감
+    public int solution(int n) {
+        int num = 1;
+        for (int i = 1; i < n; i++) {
             num++;
+            while (num % 3 == 0 || (num + "").indexOf("3") > -1) num++;
         }
+        return num;
     }
-    return num;
-}
 }
