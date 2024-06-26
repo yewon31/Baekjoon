f6b5aa8cb9f5720e184c20ecd690d1045ffebbf4
Yewon Cho <qjqmti0113@gmail.com>
2024-06-26 14:44:08 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -2,8 +2,8 @@ class Solution {
     // chicken - 시켜먹은 치킨의 수
     // 받을 수 있는 최대 서비스 치킨의 수 반환
     public int solution(int chicken) {
-        int serviceCnt = 0;
-        if(chicken > 9) serviceCnt += 1 + (chicken - 10) / 9;
+        int serviceCnt = chicken / 9;
+        if (chicken > 1 && chicken % 9 == 0) serviceCnt--;
         return serviceCnt;
     }
 }
