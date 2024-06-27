48fa700875ccfdf7f739f0052a9e117f51dc4d98
Yewon Cho <qjqmti0113@gmail.com>
2024-06-27 22:30:13 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.15 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,12 +3,15 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+    // l~r 정수 중에서 "0"과 "5"로만 이루어진 정수를 오름차순으로 저장한 배열 반환
     public int[] solution(int l, int r) {
         List<Integer> list = new ArrayList<>();
-        int start = (l % 5 == 0) ? l : (l / 5) * 5 + 5;
-        for (int i = start; i <= r; i += 5) {
-            if ((i + "").replaceAll("[05]", "").length() == 0) list.add(i);
+        int i = 0;
+        while (++i > 0) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5; // 1, 10, 11, 100 ...
+            if (l <= num && num <= r) list.add(num);
+            else if (num > r) break;
         }
-        return !list.isEmpty() ? list.stream().mapToInt(i -> i).toArray() : new int[]{-1};
+        return !list.isEmpty() ? list.stream().mapToInt(Integer::intValue).toArray() : new int[]{-1};
     }
 }
