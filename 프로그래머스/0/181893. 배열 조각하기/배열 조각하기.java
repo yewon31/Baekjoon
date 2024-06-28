74511dd2a36df78de2260087c94cf8550336a09f
Yewon Cho <qjqmti0113@gmail.com>
2024-06-28 09:31:17 +0900

    [level 0] Title: 배열 조각하기, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         int idxS = 0;
         int idxE = arr.length - 1;
         for (int i = 0; i < query.length; i++) {
-            int idx = query[i];
-            if (i % 2 == 0) idxE = Math.min(idxS + idx, idxE);
-            else idxS = Math.max(idxS + idx, idxS);
+            int idx = idxS + query[i];
+            if (i % 2 == 0) idxE = idx;
+            else idxS = idx;
         }
         return Arrays.copyOfRange(arr, idxS, idxE + 1);
     }
