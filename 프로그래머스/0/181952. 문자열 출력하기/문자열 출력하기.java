e9408db7d888eb37ecbeb71a85dc2c03609ef0d2
Yewon Cho <qjqmti0113@gmail.com>
2024-06-28 11:19:47 +0900

    [level 0] Title: 문자열 출력하기, Time: 216.71 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    // str 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
+    }
+}
