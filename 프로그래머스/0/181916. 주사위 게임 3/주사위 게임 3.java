bdf0b8c237b995296bbdad80dbe2d7e1e771bddf
Yewon Cho <qjqmti0113@gmail.com>
2024-07-02 00:15:41 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.32 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,37 +1,22 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
     // 네 주사위를 굴렸을 때 나온 숫자가 주어질 때 얻는 점수 반환
     public int solution(int a, int b, int c, int d) {
-        List<Integer> numList = new ArrayList<>(Arrays.asList(a, b, c, d));
-        numList = numList.stream().sorted().collect(Collectors.toList());
-        Set<Integer> set = new HashSet<>(numList);
-        if (set.size() == 1) { // 4 전부 동일
-            return numList.get(0) * 1111;
-        }
-        if (set.size() == 2) { // 3:1 or 2:2
-            if (numList.get(1) == numList.get(2)) { // 3:1(p:q) => (10*p+q)^2
-                int p = numList.get(1);
-                for (int i = 0; i < 3; i++) numList.remove(Integer.valueOf(p)); // Integer 객체로 변환 -> remove(Object o) 호출
-                int q = numList.get(0);
-                return (10 * p + q) * (10 * p + q);
-            }
-            // 2:2
-            int p = numList.get(0);
-            int q = numList.get(2);
-            return (p + q) * (q - p);
-        }
-        if (set.size() == 3) { // 2:1:1(p:q:r) -> q*r
-            List<Integer> duplRemoveList = new ArrayList<>();
-            for (Integer num : numList) {
-                if (duplRemoveList.contains(num)) {
-                    duplRemoveList.remove(num);
-                } else duplRemoveList.add(num);
-            }
-            return duplRemoveList.get(0) * duplRemoveList.get(1);
-        }
-        // 1:1:1:1
-        return numList.get(0);
+        // 값 정렬 후 재할당
+        int[] dice = {a, b, c, d}; Arrays.sort(dice);
+        a = dice[0]; b = dice[1]; c = dice[2]; d = dice[3];
+        // [1] 4:0
+        if (a == d) return a * 1111;
+        // [2] 3:1(p:q) => (10*p+q)^2
+        if (a == c || b == d) return (b * 10 + (a + d - b)) * (b * 10 + (a + d - b));
+        // [3] 2:2
+        if (a == b && c == d) return (a + c) * (c - a);
+        // [4] 2:1:1(p:q:r) -> q*r
+        if (a == b) return c * d;
+        if (b == c) return a * d;
+        if (c == d) return a * b;
+        // [5] 1:1:1:1
+        return a;
     }
 }
