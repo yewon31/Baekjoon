0d6789eacbf9f256f41c49a9016c714e7e51a30d
Yewon Cho <qjqmti0113@gmail.com>
2024-07-01 17:34:09 +0900

    [level 0] Title: OX퀴즈, Time: 7.45 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -3,22 +3,11 @@ import java.util.Arrays;
 class Solution {
     // 수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열 반환
     public String[] solution(String[] quiz) {
-        int len = quiz.length;
-        String[] verifyArr = new String[len];
-        for (int i = 0; i < len; i++) {
-            String[] arr = quiz[i].split(" = ");
-            verifyArr[i] = verify(arr[0], arr[1]);
+        for (int i = 0; i < quiz.length; i++) {
+            String[] text = quiz[i].split(" "); // a + b = c
+            int result = Integer.parseInt(text[0]) + (Integer.parseInt(text[2]) * (text[1].equals("+") ? 1 : -1));
+            quiz[i] = ((result + "").equals(text[4])) ? "O" : "X";
         }
-        return verifyArr;
-    }
-
-    public String verify(String exp, String resultStr) { // "3 - 4 = -3"
-        String[] arr = exp.split(" [+-] ");
-        int a = Integer.parseInt(arr[0]);
-        int b = Integer.parseInt(arr[1]);
-        int result = 0;
-        if (exp.indexOf("+") > 0) result = a + b;
-        else result = a - b;
-        return (result + "").equals(resultStr) ? "O" : "X";
+        return quiz;
     }
 }
