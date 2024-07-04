5eb50a54615064e56759c95193b4929d7e0149b8
Yewon Cho <qjqmti0113@gmail.com>
2024-07-05 00:09:15 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,27 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
+
     // 네 개의 점을 두 개씩 이었을 때, 두 직선이 평행이 되는 경우가 있으면 1
+    int[][] dots;
+
     public int solution(int[][] dots) {
-        // 이차원 배열을 map에 저장 -> {0, {x:0, {y:0}} ...
-        Map<Integer, Map<String, Integer>> map = new HashMap<>();
-        for (int i = 0; i < dots.length; i++) {
-            map.put(i, Map.of("x", dots[i][0], "y", dots[i][1]));
-        }
-        // 두 직선이 평행이 되는지 체크(비율 확인)
-        if ((getRatio(map.get(0), map.get(1)) == getRatio(map.get(2), map.get(3)))
-                || (getRatio(map.get(0), map.get(2)) == getRatio(map.get(1), map.get(3)))
-                || (getRatio(map.get(0), map.get(3)) == getRatio(map.get(1), map.get(2)))) {
-            return 1;
-        }
+        this.dots = dots;
+        if (checkParallel(0, 1, 2, 3)) return 1;
+        if (checkParallel(0, 2, 1, 3)) return 1;
+        if (checkParallel(0, 3, 1, 2)) return 1;
         return 0;
     }
 
-    // 점(좌표) 두개를 이었을 때의 비율(세로/가로)
-    public double getRatio(Map<String, Integer> mapA, Map<String, Integer> mapB) {
-        int width = Math.abs(mapA.get("x") - mapB.get("x"));
-        int height = Math.abs(mapA.get("y") - mapB.get("y"));
-        return (double) height / width;
+    boolean checkParallel(int a, int b, int c, int d) {
+        double ratioAB = (double) (dots[a][1] - dots[b][1]) / (dots[a][0] - dots[b][0]); // a와 b를 이은 직선의 기울기(y/x)
+        double ratioCD = (double) (dots[c][1] - dots[d][1]) / (dots[c][0] - dots[d][0]);
+        return ratioAB == ratioCD || ratioAB == -ratioCD;
     }
 }
