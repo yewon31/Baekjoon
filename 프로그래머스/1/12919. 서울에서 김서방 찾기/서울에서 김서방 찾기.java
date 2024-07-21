db34729fdce4e34d0a39521ec10fdb026a8a8edd
Yewon Cho <qjqmti0113@gmail.com>
2024-07-21 22:20:32 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 11.80 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String[] seoul) {
-        int index = -1;
         for (int i = 0; i < seoul.length; i++) {
             if (seoul[i].equals("Kim")) return "김서방은 " + i + "에 있다";
         }
         return "";
     }
-}
+    }
