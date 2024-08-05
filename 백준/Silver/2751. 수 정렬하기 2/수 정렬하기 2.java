d22de5c9476f4d498407a6edec4c832e8fd2da2a
Yewon Cho <qjqmti0113@gmail.com>
2024-08-05 21:25:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 664 ms, Memory: 94112 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,17 +1,17 @@
 import java.io.*;
-import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 1 ≤ N ≤ 1,000,000
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine()); // 절댓값이 1,000,000보다 작거나 같은 정수
-        }
-        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
-        for (int value : arr) sb.append(value).append('\n');
-        System.out.println(sb);
+        int N = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[2000001];
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) sb.append((i - 1000000)).append('\n');
+        }
+        System.out.print(sb);
     }
 }
