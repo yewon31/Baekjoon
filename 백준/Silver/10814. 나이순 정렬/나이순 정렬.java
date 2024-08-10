297d7cd1fab2ff9d90dbe844ec6e9fc2ebf44ada
Yewon Cho <qjqmti0113@gmail.com>
2024-08-10 17:35:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 504 ms, Memory: 56180 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,21 +6,20 @@ public class Main {
         /* [1] 입력 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 온라인 저지 회원의 수(1 ≤ N ≤ 100,000)
-        String[][] arrList = new String[N][2]; // 나이, 이름 저장
+        StringBuilder[] memberList = new StringBuilder[201];
         for (int i = 0; i < N; i++) {
             String[] arr = br.readLine().split(" ");
-            arrList[i][0] = arr[0]; // 나이 : 1보다 크거나 같으며, 200보다 작거나 같은 정수
-            arrList[i][1] = arr[1]; // 이름 : 알파벳 대소문자로 이루어져 있고, 길이가 100보다 작거나 같은 문자열
+            int age = Integer.parseInt(arr[0]); // 나이 : 1보다 크거나 같으며, 200보다 작거나 같은 정수
+            String name = arr[1]; // 이름 : 알파벳 대소문자로 이루어져 있고, 길이가 100보다 작거나 같은 문자열
+            if (memberList[age] == null) memberList[age] = new StringBuilder();
+            memberList[age].append(age + " " + name + "\n");
         }
 
-        /* [2]정렬(나이 순 -> 가입한 순) */
-        Arrays.sort(arrList, (a, b) -> Integer.parseInt(a[0]) - Integer.parseInt(b[0]));
-
-        /* [3] 출력 */
-        StringBuilder sb = new StringBuilder();
-        for (String[] arr : arrList) {
-            sb.append(arr[0] + " " + arr[1] + "\n");
+        /* [2] 출력 */
+        StringBuilder sbList = new StringBuilder();
+        for (StringBuilder sb : memberList) {
+            if (sb != null) sbList.append(sb.toString());
         }
-        System.out.println(sb.toString());
+        System.out.println(sbList.toString());
     }
 }
