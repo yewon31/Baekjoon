3542b1ea27b98fa67a5784e46f62c95635cd0d65
Yewon Cho <qjqmti0113@gmail.com>
2024-08-13 01:22:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 824 ms, Memory: 55860 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -3,35 +3,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+
         /* [1] 입력 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 2차원 평면 위 점의 개수 N (1 ≤ N ≤ 100,000)
-        
-        // 점들을 저장할 리스트
-        List<int[]> points = new ArrayList<>();
-        
+        int[][] arrList = new int[N][2];
         for (int i = 0; i < N; i++) {
-            String[] xy = br.readLine().split(" "); // i번점의 위치 xi와 yi(-100,000 ≤ xi, yi ≤ 100,000)
-            int x = Integer.parseInt(xy[0]);
-            int y = Integer.parseInt(xy[1]);
-            points.add(new int[] {x, y});
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arrList[i][0] = Integer.parseInt(st.nextToken());
+            arrList[i][1] = Integer.parseInt(st.nextToken());
         }
-        
+
         /* [2] 정렬 */
-        points.sort((p1, p2) -> {
-            if (p1[0] == p2[0]) {
-                return Integer.compare(p1[1], p2[1]); // x좌표가 같으면 y좌표를 기준으로 오름차순 정렬
-            } else {
-                return Integer.compare(p1[0], p2[0]); // x좌표 기준 오름차순 정렬
-            }
-        });
-        
+        Arrays.sort(arrList, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+
         /* [3] 출력 */
         StringBuilder sb = new StringBuilder();
-        for (int[] point : points) {
-            sb.append(point[0]).append(" ").append(point[1]).append("\n");
+        for (int i = 0; i < N; i++) {
+            sb.append(arrList[i][0] + " " + arrList[i][1]).append('\n');
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
