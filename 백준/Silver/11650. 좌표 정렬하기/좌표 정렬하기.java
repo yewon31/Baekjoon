6084953b610e7b52df232591b440f3a093744c24
Yewon Cho <qjqmti0113@gmail.com>
2024-08-13 01:33:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 700 ms, Memory: 61240 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -15,7 +15,11 @@ public class Main {
         }
 
         /* [2] 정렬 */
-        Arrays.sort(arrList, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        Arrays.sort(arrList, Comparator.comparingInt((int[] a) -> a[0]).thenComparingInt(a -> a[1]));
+        // (int[] a) -> a[0] 이 람다식은 배열 arr의 각 요소(즉, int[])를 받아서 그 배열의 첫 번째 요소(a[0])를 반환합니다. 따라서 arr의 요소들은 a[0]에 따라 정렬됩니다.
+        // .thenComparingInt(a -> a[1]): 이 부분은 추가적인 정렬 기준을 지정합니다. 
+        // thenComparingInt 메서드는 첫 번째 정렬 기준(즉, a[0])이 동일한 경우에 두 번째 기준(즉, a[1])을 사용하여 정렬을 수행합니다.
+        // a -> a[1] 이 람다식은 배열 a를 받아서 그 배열의 두 번째 요소(a[1])를 반환합니다. 여기서 a의 타입은 생략되었지만, 자바의 타입 추론에 의해 int[]로 인식됩니다.
 
         /* [3] 출력 */
         StringBuilder sb = new StringBuilder();
