224373c6dc04ce43769000ae5e5d8c669ee401be
Yewon Cho <qjqmti0113@gmail.com>
2024-08-16 17:57:24 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 33.81 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,21 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.stream.Collectors;
 
 class Solution {
     // 음식의 양이 칼로리가 적은 순서대로 주어졌을 때, 음식의 배치를 나타내는 문자열 반환
     public String solution(int[] food) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.offer(0); // 물 배치
+        String str = "0"; // 물 배치
         for (int i = food.length - 1; i > 0; i--) { // 칼로리가 많은 음식부터 안쪽에 배치
             int amount = food[i];
             for (int j = 0; j < amount / 2; j++) {
-                deque.offerFirst(i); // 왼쪽 끝에 배치
-                deque.offerLast(i); // 오른쪽 끝에 배치
+                str = i + str + i; // 왼쪽/오른쪽 끝에 배치
             }
         }
-        return deque.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(""));
+        return str;
     }
 }
